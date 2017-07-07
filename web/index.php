029565ca2f03b336c676a015c234e3df5ab4d1c8
Vincent Dechenaux <vincent@wizaplace.com>
2017-07-07 16:55:25 +0200

    Move cron to silex

--- web/index.php
+++ web/index.php
@@ -1,5 +1,10 @@
 <?php
 
+use Doctrine\Common\Cache\FilesystemCache;
+use GuzzleHttp\HandlerStack;
+use Kevinrob\GuzzleCache\CacheMiddleware;
+use Kevinrob\GuzzleCache\Storage\DoctrineCacheStorage;
+use Kevinrob\GuzzleCache\Strategy\PrivateCacheStrategy;
 use Symfony\Component\HttpFoundation\Cookie;
 
 require_once __DIR__.'/../vendor/autoload.php';
@@ -19,6 +24,30 @@ $app['templating'] = new class() {
 };
 
 require_once __DIR__.'/../functions.php';
+$config = include __DIR__.'/../config.php';
+
+$guzzleOptions = [];
+
+$stack = HandlerStack::create();
+$stack->push(
+    new CacheMiddleware(
+        new PrivateCacheStrategy(
+            new DoctrineCacheStorage(
+                new FilesystemCache('/tmp/team-reviewer')
+            )
+        )
+    ),
+    'cache'
+);
+
+$guzzleOptions['handler'] = $stack;
+
+if (isset($config['auth']['token'])) {
+    $guzzleOptions['headers']['Authorization'] = 'token '.$config['auth']['token'];
+} elseif (isset($config['auth']['basic'])) {
+    $guzzleOptions['auth'] = [$config['auth']['basic']['username'], $config['auth']['basic']['password']];
+}
+$app['http.client'] = new \GuzzleHttp\Client($guzzleOptions);
 
 $app->get('/', function () use ($app) {
     if (!empty($_GET['id']) && !empty($_GET['redir'])) {
@@ -41,6 +70,36 @@ $app->get('/{id}/{redir}', function ($id, $redir) use ($app) {
     $response->headers->setCookie(new Cookie('lastClick['.$id.']', (string) time(), time() + 2678400));
 
     return $response;
+});
+
+$app->get('/update', function () use ($app, $config) {
+    $repos = [];
+
+    foreach ($config['repositories'] as $repository) {
+        $response = $app['http.client']->get('https://api.github.com/repos/' . $repository . '/pulls?state=open&per_page=100');
+
+        $response = json_decode($response->getBody()->getContents(), true);
+
+        foreach ($response as &$pr) {
+            echo "  #{$pr['number']} {$pr['title']}\n";
+
+            $pr['issue'] = json_decode(
+                $app['http.client']->get($pr['issue_url'])->getBody()->getContents(),
+                true
+            );
+
+            $pr['status'] = json_decode(
+                $app['http.client']->get('https://api.github.com/repos/'.$repository.'/commits/'.$pr['head']['sha'].'/status')->getBody()->getContents(),
+                true
+            );
+        }
+
+        $repos[$repository] = $response;
+    }
+
+    file_put_contents(__DIR__.'/../repos.dat', serialize($repos));
+
+    return 'ok';
 });
 
 $app->run();
